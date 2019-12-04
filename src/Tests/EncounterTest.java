5066768df824b0b74353bbe8ec3e13645d874d1e
davidc-gv <54907152+davidc-gv@users.noreply.github.com>
2019-12-04 18:51:34 -0500

    Added testing capabilities

---
+++ src/Tests/EncounterTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EncounterTest {
+
+    @Test
+    public void nextTurn() {
+    }
+
+    @Test
+    public void removeChar() {
+    }
+
+    @Test
+    public void addMonster() {
+    }
+
+    @Test
+    public void addMonsterCR() {
+    }
+}
